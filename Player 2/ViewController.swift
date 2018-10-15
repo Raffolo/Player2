8c8f710d0d81840dcbf9fcfe966fecca7a42fa4b
Michele Marras <michelemarras@10-20-49-161.0200.fed.it.iosda.org>
2018-10-15 10:32:42 +0200

    Initial Commit

---
+++ Player 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Player 2
+//
+//  Created by Michele Marras on 15/10/2018.
+//  Copyright © 2018 Michele Marras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
